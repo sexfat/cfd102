273dbfd912899e097fc81fff3d5501ac6a76c1e2
sexfat <bryant6831@gmail.com>
2021-07-20 09:38:28 +0800

    basic

--- animation.js
+++ animation.js
@@ -1,3 +1,6 @@
 // start
-
+TweenMax.to('.basic' , 1 , {x: 100 , y: 100})
+TweenMax.from('.basic2' , 1 , {x: 100 , y: 100})
+TweenMax.set('.basic3'  , {x: 600 , y: 600})
+TweenMax.fromTo('.basic4' ,1, {x : 100 , y : 100} ,{x: 400, y: 500})
 
