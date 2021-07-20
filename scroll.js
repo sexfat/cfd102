97ef2aa3869f1e11fd0b9612bbc37068a3338ab5
sexfat <bryant6831@gmail.com>
2021-07-20 15:28:43 +0800

    add

--- scroll.js
+++ scroll.js
@@ -4,10 +4,9 @@ var controller = new ScrollMagic.Controller();
 //動態
 var mv01 =  TweenMax.to('.mv01' , 1 ,{
    x: 400,
-   y: 400
+   y: 400,
+   rotation : 360
 });
-
-
 
 // 觸發事件
 new ScrollMagic.Scene({
