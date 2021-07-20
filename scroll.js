869ae690ef00e2513f85bc70413f53c7413a2427
sexfat <bryant6831@gmail.com>
2021-07-20 15:42:19 +0800

    scroll 時間軸動畫

--- scroll.js
+++ scroll.js
@@ -10,7 +10,10 @@ var mv01 =  TweenMax.to('.mv01' , 1 ,{
 
 // 觸發事件
 new ScrollMagic.Scene({
-    triggerElement : '#trigger01'// 觸發點  
+    triggerElement : '#trigger01',// 觸發點  
+    // offset: '100px', // 改變觸發位置 
+    triggerHook: '.5', // 0 ~ 1
+    duration: '100%' // 時間軸
 })
 .setTween(mv01) // 動畫
 .addIndicators() // 觸發指標
