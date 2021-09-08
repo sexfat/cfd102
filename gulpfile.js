9c548ddab94462df28c302d4db728f68213c5ebb
sexfat <bryant6831@gmail.com>
2021-09-08 10:05:23 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const {
+    src,
+    dest,
+    series,
+    parallel,
+    watch
+} = require('gulp');
+
+
+// 任務一 
+function defaultask(cb) {
+    console.log('gulp ok');
+    cb();
+}
+
+exports.do = defaultask;
+
+
+
+function missionA(cb) {
+    console.log('missionA');
+    cb();
+}
+
+function missionB(cb) {
+    console.log('missionB');
+    cb();
+}
+
+function missionC(cb) {
+    console.log('missionC');
+    cb();
+}
+
+function missionD(cb) {
+    console.log('missionD');
+    cb();
+}
+
+// 順序
+exports.async = series(missionA , missionB);
+
+//先執行A任務 在同時執行 B 與 C 任務 在執行D
+exports.mix = series(missionA ,  parallel(missionB , missionC) , missionD);
+
+// 同時
+exports.sync = parallel(missionA , missionB);
