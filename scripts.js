a25cd43229e5349569f435681fc20c5f269b80c5
sexfat <bryant6831@gmail.com>
2021-09-14 09:51:59 +0800

    add

---
+++ scripts.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+$('body').css('background-color' , 'green');
+
+
+let arr = [1 ,2 ,3];
+console.log(arr);
+console.log('ok');
+
+var p = function(a , b ){
+  return a + b ;
+}
+
+console.log(p(2 ,4));
